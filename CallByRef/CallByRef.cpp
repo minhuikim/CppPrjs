f1c600026505e2527ac58be118821e693b8e45fc
미니 <kminh940@naver.com>
2021-11-06 23:34:08 +0900

    출석수업 과제물 7. 참조호출 8. 함수 다중정의 예제

--- CallByRef/CallByRef.cpp
+++ CallByRef/CallByRef.cpp
@@ -7,6 +7,23 @@ struct Circle {
 	float radius, cx, cy;
 };
 
+struct Rectangle {
+	float x1, y1;
+	float x2, y2;
+};
+
+void inputData(Rectangle& r)
+{
+	cout << "좌측 하단 (x1) : ";
+	cin >> r.x1;
+	cout << "좌측 하단 (y1) : ";
+	cin >> r.y1;
+	cout << "우측 상단 (x2) : ";
+	cin >> r.x2;
+	cout << "우측 상단 (y2) : ";
+	cin >> r.y2;
+}
+
 void inputData(Circle& c)
 {
 	cout << "원의 반지름 : ";
@@ -17,11 +34,24 @@ void inputData(Circle& c)
 	cin >> c.cy;
 }
 
+void prData(const Rectangle& r)
+{
+	cout << "좌측 하단 좌표 = (" << r.x1 << ", " << r.y1 << ")" << endl;
+	cout << "우측 상단 좌표 = (" << r.x2 << ", " << r.y2 << ")" << endl;
+	cout << "가로길이 = " << (r.x2 - r.x1) << endl;
+	cout << "세로길이 = " << (r.y2 - r.y1) << endl;
+}
+
 void prData(const Circle& c)
 {
 	cout << "반지름 = " << c.radius << endl;
 	cout << "중심좌표 = (" << c.cx;
 	cout << ", " << c.cy << ")" << endl;
+}
+
+float area(const Rectangle& r)
+{
+	return (r.x2-r.x1) * (r.y2-r.y1);
 }
 
 float area(const Circle& c)
@@ -31,9 +61,15 @@ float area(const Circle& c)
 
 int main()
 {
-	Circle circle = { 1, 2, 3 };
+	Circle circle = {1, 2, 3};
 	inputData(circle);
 	cout << "입력된 원의 정보" << endl;
 	prData(circle);
 	cout << "원의 면적 = " << area(circle) << endl;
+
+	Rectangle ractangle = { 1, 2, 3, 4 };
+	inputData(ractangle);
+	cout << "입력된 좌표의 정보" << endl;
+	prData(ractangle);
+	cout << "사각형의 면적 = " << area(ractangle) << endl;
 }
