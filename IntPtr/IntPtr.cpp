44e3a2cd25ce493d1a63f3886d147bca59eadc55
미니 <kminh940@naver.com>
2021-11-06 19:38:37 +0900

    출석수업 과제물

---
+++ IntPtr/IntPtr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "202134-365627 김민희" << endl;
+
+	int a = 10, b = 100;
+	int* ptr = &a;
+
+	cout << "ptr이 가리키는 곳의 값 : " << *ptr << endl;
+	*ptr = 20;
+	cout << "변수 a의 값 : " << a << endl;
+	ptr = &b;
+	cout << "변수 b의 값 : " << *ptr << endl;
+
+	return 0;
+}
