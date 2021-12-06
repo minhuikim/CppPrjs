7cbdfd0978890111b09ed1c66a11f7583e479463
미니 <kminh940@naver.com>
2021-12-06 23:16:25 +0900

    Counter Header

---
+++ Project1/Counter.h
@@ -0,0 +1,16 @@
+#ifndef COUNTER_H_INCLUDED	// Counter.h가 중복 include 되지 않았는지 검사
+
+#define COUNTER_H_INCLUDED	// Counter.h가 처음 include될 때 정의됨
+
+class Counter {				// class Counter의 선언 시작
+	int value;				// private 데이터 멤버
+public :					// public 멤버 함수
+	void reset()			// 계수기의 값을 0으로 지움
+		{ value = 0; }
+	void count()			// 계수기의 값을 1 증가시킴
+		{ ++value; }
+	void getValue() const	// 계수기의 현재 값을 반환함
+		{ return value; }
+};
+
+#endif // COUNTER_H_INCLUDED
