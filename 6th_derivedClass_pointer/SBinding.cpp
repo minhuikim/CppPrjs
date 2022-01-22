1152a1e6bb8ce641647956dbd974057f8b204472
미니 <kminh940@naver.com>
2022-01-22 18:33:17 +0900

    6th_virtual_function

--- 6th_derivedClass_pointer/SBinding.cpp
+++ 6th_derivedClass_pointer/SBinding.cpp
@@ -10,6 +10,10 @@ int main()
 	cout << endl;
 	Person* p2 = new Student("Harry", "Hogwarts");
 	p2->print(); cout << endl;			// Person::print() 호출
+	/*
+	 실제로 연결된 객체는 Student 클래스 이지만 Person포인터를 통해 호출되므로 
+	 12행에서 호출되는 함수는 Person::print();이다.
+	*/
 	((Student*)p2)->print();			// Student::print() 호출
 	cout << endl;
 	return 0;
