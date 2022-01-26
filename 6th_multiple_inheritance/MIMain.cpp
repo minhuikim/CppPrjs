0bfd6a5cfcd286ab4bf4c3ad47330c02cfef0bca
미니 <kminh940@naver.com>
2022-01-26 22:50:21 +0900

    6th_multiple_inheritance

---
+++ 6th_multiple_inheritance/MIMain.cpp
@@ -0,0 +1,15 @@
+#include "MIParttime.h"
+
+int main()
+{
+	Parttime chulsoo("ABC Univ.", "DEF Co.");
+	chulsoo.printSchool();		// Student의 멤버함수 호출
+	chulsoo.printCompany();		// Employee의 멤버함수 호출
+	return 0;
+}
+
+/*
+[출력]
+ABC Univ.
+DEF Co.
+*/
