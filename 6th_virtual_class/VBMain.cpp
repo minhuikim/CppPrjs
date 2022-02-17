2430da0b8316eccf5f6533c74db47e27658be779
미니 <kminh940@naver.com>
2022-02-17 23:48:40 +0900

    6th_virtual_class

---
+++ 6th_virtual_class/VBMain.cpp
@@ -0,0 +1,14 @@
+#include "VBParttime.h"
+
+int main()
+{
+	Parttime chulsoo("Chulsoo", "ABC Univ", "DEF co.");
+	chulsoo.print();
+	return 0;
+}
+
+/*
+[Ãâ·Â]
+Chulsoo goes to ABC Univ
+Chulsoo is employed by DEF co.
+*/
